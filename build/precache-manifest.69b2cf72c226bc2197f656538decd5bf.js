01fdf48cf4b9a2cb2e5c1dfae0772af14467039a
udit <uditshah36@gmail.com>
2020-02-25 22:32:19 +0530

    poublic to build

---
+++ build/precache-manifest.69b2cf72c226bc2197f656538decd5bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b41d90396be8fc8f3d2153c3ca41644",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65dcd32a4a9b06a05819",
+    "url": "/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "e1406a217ce6718d7fdd",
+    "url": "/static/js/2.8e686651.chunk.js"
+  },
+  {
+    "revision": "7f84d11092c10d3a43893d78f68c23cb",
+    "url": "/static/js/2.8e686651.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65dcd32a4a9b06a05819",
+    "url": "/static/js/main.29730a47.chunk.js"
+  },
+  {
+    "revision": "2744f5b910f9765f6d95",
+    "url": "/static/js/runtime-main.8d9e579c.js"
+  }
+]);
